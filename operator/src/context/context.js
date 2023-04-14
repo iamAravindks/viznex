652455def1b7c7e9457190a3417a02f235b47d64
iamAravindks <aravindkadaikattu@gmail.com>
2023-04-14 13:07:12 +0530

    operator

---
+++ operator/src/context/context.js
@@ -0,0 +1,196 @@
+import { createContext, useReducer } from "react";
+import { contextReducer } from "./contextReducer";
+import {
+  CLEAR_ERROR,
+  CLEAR_LOADING,
+  CREATE_QUEUE,
+  LOAD_DEVICES,
+  REQUEST,
+  SET_ERROR,
+  SET_LOADING,
+  USER_AUTH_FAIL,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT_SUCCESS,
+  USER_PROFILE_SUCCESS,
+} from "./types";
+import axios from "axios";
+
+const userStatus = localStorage.getItem("Viznx_operator_Status") || null;
+console.log(userStatus);
+const initialState = {
+  loading: false,
+  userInfo: null,
+  error: null,
+  userStatus,
+  devices: [],
+};
+
+const BASE_URL = "https://viznexdev.me/api/operator";
+
+export const Context = createContext(initialState);
+const token = localStorage.getItem("Viznx_Secure_Session_ID");
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+  withCredentials: true,
+};
+
+const Provider = ({ children }) => {
+  const [userState, dispatch] = useReducer(contextReducer, initialState);
+
+  // @actions
+
+  // login
+
+  const login = async (email, password) => {
+    try {
+      dispatch({ type: REQUEST });
+
+      const res = await axios.post(
+        `${BASE_URL}/login`,
+        { email, password },
+        config
+      );
+      // if (
+      //   res?.data?.Viznx_Secure_Session_ID &&
+      //   res?.data?.Viznx_operator_Status
+      // ) {
+      //   localStorage.setItem(
+      //     "Viznx_Secure_Session_ID",
+      //     res.data.Viznx_Secure_Session_ID
+      //   );
+
+      //   localStorage.setItem(
+      //     "Viznx_operator_Status",
+      //     res.data.Viznx_operator_Status
+      //   );
+      // }
+
+      dispatch({ type: USER_LOGIN_SUCCESS, payload: res.data });
+    } catch (error) {
+      dispatch({
+        type: USER_AUTH_FAIL,
+      });
+      const err =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      console.log(error);
+      dispatch({
+        type: SET_ERROR,
+        payload: err,
+      });
+    }
+  };
+
+  // load profile
+
+  const loadProfile = async () => {
+    try {
+      dispatch({ type: REQUEST });
+
+      const res = await axios.get(`${BASE_URL}/profile`, config);
+
+      console.log(res);
+      dispatch({ type: USER_PROFILE_SUCCESS, payload: res.data });
+    } catch (error) {
+      dispatch({
+        type: USER_AUTH_FAIL,
+      });
+      const err =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      console.log(err);
+      dispatch({
+        type: SET_ERROR,
+        payload: err,
+      });
+    }
+  };
+
+  // error remover
+
+  const clearError = () => {
+    dispatch({ type: CLEAR_ERROR });
+  };
+
+  const setLoading = (set) => {
+    if (set) {
+      dispatch({ type: SET_LOADING });
+    } else dispatch({ type: CLEAR_LOADING });
+  };
+
+  //logout
+  const logout = async () => {
+    try {
+      localStorage.removeItem("Viznx_Secure_Session_ID");
+      localStorage.removeItem("Viznx_operator_Status");
+      dispatch({ type: USER_LOGOUT_SUCCESS });
+    } catch (error) {
+      const err =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      dispatch({ type: SET_ERROR, payload: err });
+    }
+  };
+
+  // load devices
+
+  const loadDevices = async () => {
+    try {
+      dispatch({ type: REQUEST });
+      const res = await axios.get(`${BASE_URL}/load-devices`, config);
+      console.log(res);
+      dispatch({ type: LOAD_DEVICES, payload: res?.data });
+    } catch (error) {
+      const err =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      dispatch({ type: SET_ERROR, payload: err });
+    }
+  };
+
+  //add ad
+
+  const addAd = async (obj) => {
+    try {
+      dispatch({ type: REQUEST });
+      const res = await axios.post(`${BASE_URL}/create-queue`, obj, config);
+      dispatch({ type: CREATE_QUEUE });
+    } catch (error) {
+      const err =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      dispatch({ type: SET_ERROR, payload: err });
+    }
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        userInfo: userState.userInfo,
+        loading: userState.loading,
+        error: userState.error,
+        userStatus: userState.userStatus,
+        devices: userState.devices,
+        login,
+        clearError,
+        loadProfile,
+        logout,
+        setLoading,
+        loadDevices,
+        addAd,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export default Provider;
---
+++ operator/src/context/contextReducer.js
@@ -0,0 +1,79 @@
+import {
+  CLEAR_ERROR,
+  CLEAR_LOADING,
+  CREATE_QUEUE,
+  LOAD_DEVICES,
+  REQUEST,
+  SET_ERROR,
+  SET_LOADING,
+  USER_AUTH_FAIL,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT_SUCCESS,
+  USER_PROFILE_SUCCESS,
+} from "./types";
+
+export const contextReducer = (state, action) => {
+  switch (action.type) {
+    case REQUEST:
+      return { ...state, loading: true };
+    case USER_LOGIN_SUCCESS:
+      localStorage.setItem(
+        "Viznx_Secure_Session_ID",
+        action.payload.Viznx_Secure_Session_ID
+      );
+
+      localStorage.setItem(
+        "Viznx_operator_Status",
+        action.payload.Viznx_operator_Status
+      );
+      return {
+        ...state,
+        loading: false,
+        userInfo: action.payload.operator,
+        error: null,
+      };
+    case USER_PROFILE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        userInfo: action.payload,
+        error: null,
+      };
+    case USER_AUTH_FAIL:
+      return { ...state, loading: false, userInfo: null };
+    case SET_ERROR:
+      return { ...state, error: action.payload };
+    case CLEAR_ERROR:
+      return { ...state, error: null };
+    case USER_LOGOUT_SUCCESS:
+      return {
+        ...state,
+        userInfo: null,
+        loading: false,
+      };
+
+    case LOAD_DEVICES:
+      return {
+        ...state,
+        loading: false,
+        devices: action.payload,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case CLEAR_LOADING:
+      return {
+        ...state,
+        loading: false,
+      };
+    case CREATE_QUEUE:
+      return {
+        ...state,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
